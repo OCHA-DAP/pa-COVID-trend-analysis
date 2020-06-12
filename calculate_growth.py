1e986129ee34982b791a89aad200e1057472bc8f
Leonardo Milano <milanoleonardo@yahoo.it>
2020-06-12 09:59:31 +0200

    30 day window

--- calculate_growth.py
+++ calculate_growth.py
@@ -15,7 +15,7 @@ FILENAME_SHP = 'ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp'
 # use 15 days as reference with error bands from 7 and 30 days
 # additional uncertainity from comparison between fit and counts
  #TIME_RANGE={'mid': 15, 'min': 7, 'max': 30}
-TIME_RANGE={'mid': 7}
+TIME_RANGE={'mid': 30}
 
 # Suppress warnings that come from the doubling time calc
 np.seterr(divide='ignore')
