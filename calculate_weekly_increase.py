a0cd3fba03ac634b1d54c917d2507e25aa57adc0
Monica Turner <turnermon@gmail.com>
2020-06-12 13:20:55 +0200

    Added plots and output json

--- calculate_weekly_increase.py
+++ calculate_weekly_increase.py
@@ -10,6 +10,7 @@ DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 WHO_COVID_FILENAME='WHO_data/Data_ WHO Coronavirus Covid-19 Cases and Deaths - WHO-COVID-19-global-data.csv'
 POPULATION_FILENAME='Population_data/API_SP.POP.TOTL_DS2_en_excel_v2_1121005.xls'
 
+
 def main():
     # Read in list of countries
     with open('countries/admins.yaml', 'r') as stream:
@@ -22,12 +23,43 @@ def main():
     new_cases_w=df_WHO.groupby(['ISO_3_CODE']).resample('W-Mon', on='date_epicrv').sum()['NewCase']
     cumulative_w=df_WHO.groupby(['ISO_3_CODE']).resample('W-Mon', on='date_epicrv').min()['CumCase']
 
-    output_df=pd.merge(left=new_cases_w,right=cumulative_w,left_index=True,right_index=True,how='inner')
+    output_df=pd.merge(left=new_cases_w,right=cumulative_w,left_index=True,right_index=True,how='inner').reset_index()
     output_df['NewCase_Rel']=output_df['NewCase']/output_df['CumCase']
 
-    df_pop=pd.read_excel(POPULATION_FILENAME,sheet_name='Data',header=1,skiprows=[0,1],usecols='B,BK')
-    
-    print(df_pop)
+    # Read in pop
+    df_pop=pd.read_excel(POPULATION_FILENAME,sheet_name='Data',header=1,skiprows=[0,1],usecols='B,BK').rename(
+        columns={'2018': 'population'})
+    # Add HRP
+    df_pop = df_pop.append({'Country Code': 'HRP',
+                            'population':  df_pop.loc[df_pop['Country Code'].isin(HRP_iso3), 'population'].sum()},
+                           ignore_index=True)
+
+    # Add pop to output df
+    output_df = output_df.merge(df_pop, left_on='ISO_3_CODE', right_on='Country Code', how='left').drop(
+        columns=['Country Code'])
+    # Get cases per hundred thousand
+    output_df['weekly_new_cases_per_ht'] = output_df['NewCase'] / output_df['population'] * 1E5
+    output_df['weekly_inc_cases_per_ht'] = output_df['NewCase_Rel'] / output_df['population'] * 1E5
+
+    # Show plots
+    for q in ['weekly_new_cases_per_ht', 'weekly_inc_cases_per_ht']:
+        fig,axs=plt.subplots(figsize=[15,10],nrows=8,ncols=8)
+        fig.suptitle(q)
+        ifig = 0
+        for iso3, group in output_df.groupby('ISO_3_CODE'):
+            axis = axs[ifig // 8][ifig % 8]
+            axis.plot(group['date_epicrv'], group[q])
+            axis.set_title(iso3)
+            axis.set_xticks([])
+            #axis.set_ylim(0, output_df[q].max())
+            ifig += 1
+    plt.show()
+
+    # Save as JSON
+    output_df['date_epicrv'] = output_df['date_epicrv'].apply(lambda x: x.strftime('%Y-%m-%d'))
+    output_df.groupby('ISO_3_CODE').apply(lambda x: x.to_dict('r')).to_json(
+        'hrp_covid_cases.json', orient='index', indent=2)
+
 
 def get_WHO_data(HRP_iso3):
     df=pd.read_csv(f'{DIR_PATH}/{WHO_COVID_FILENAME}')
