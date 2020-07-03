cfc44fb716e982882582a7794c18e6a55a856778
Leonardo Milano <milanoleonardo@yahoo.it>
2020-07-03 12:16:46 +0200

    initial implementation

--- calculate_weekly_increase.py
+++ calculate_weekly_increase.py
@@ -20,22 +20,25 @@ def main():
     df_WHO=get_WHO_data(HRP_iso3)
     
     # get weekly new cases
-    new_cases_w=df_WHO.groupby(['ISO_3_CODE']).resample('W', on='date_epicrv').sum()['NewCase']
-    cumulative_w=df_WHO.groupby(['ISO_3_CODE']).resample('W', on='date_epicrv').min()['CumCase']
+    new_w=df_WHO.groupby(['ISO_3_CODE']).resample('W', on='date_epicrv').sum()[['NewCase','NewDeath']]
+    cumulative_w=df_WHO.groupby(['ISO_3_CODE']).resample('W', on='date_epicrv').min()[['CumCase','CumDeath']]
     ndays_w=df_WHO.groupby(['ISO_3_CODE']).resample('W', on='date_epicrv').count()['NewCase']
     ndays_w=ndays_w.rename('ndays')
 
-    output_df=pd.merge(left=new_cases_w,right=cumulative_w,left_index=True,right_index=True,how='inner')
+    output_df=pd.merge(left=new_w,right=cumulative_w,left_index=True,right_index=True,how='inner')
     output_df=pd.merge(left=output_df,right=ndays_w,left_index=True,right_index=True,how='inner')
     output_df=output_df[output_df['ndays']==7]
-    output_df=output_df[output_df['CumCase']>MIN_CUMULATIVE_CASES]
     output_df=output_df.reset_index()
-    output_df['NewCase_Rel']=output_df['NewCase']/output_df['CumCase']
-    output_df['NewCase_PercentChange'] = output_df.groupby('ISO_3_CODE')['NewCase'].pct_change()
-    # For percent change, if the diff is actually 0, change nan to 0
-    output_df['diff'] = output_df.groupby('ISO_3_CODE')['NewCase'].diff()
-    output_df.loc[(output_df['NewCase_PercentChange'].isna()) & (output_df['diff']==0), 'NewCase_PercentChange'] = 0.0
 
+    output_df['NewCase_PercentChange'] = output_df.groupby('ISO_3_CODE')['NewCase'].pct_change()
+    output_df['NewDeath_PercentChange'] = output_df.groupby('ISO_3_CODE')['NewDeath'].pct_change()
+    # For percent change, if the diff is actually 0, change nan to 0
+    output_df['diff_cases'] = output_df.groupby('ISO_3_CODE')['NewCase'].diff()
+    output_df.loc[(output_df['NewCase_PercentChange'].isna()) & (output_df['diff_cases']==0), 'NewCase_PercentChange'] = 0.0
+    output_df['diff_deaths'] = output_df.groupby('ISO_3_CODE')['NewDeath'].diff()
+    output_df.loc[(output_df['NewDeath_PercentChange'].isna()) & (output_df['diff_deaths']==0), 'NewDeath_PercentChange'] = 0.0
+
+    output_df=output_df[output_df['CumCase']>MIN_CUMULATIVE_CASES]
 
     # Read in pop
     df_pop=pd.read_excel(POPULATION_FILENAME,sheet_name='Data',header=1,skiprows=[0,1],usecols='B,BK').rename(
@@ -49,19 +52,22 @@ def main():
     output_df = output_df.merge(df_pop, left_on='ISO_3_CODE', right_on='Country Code', how='left').drop(
         columns=['Country Code'])
     # Get cases per hundred thousand
-    output_df['weekly_new_cases_per_ht'] = output_df['NewCase'] / output_df['population'] * 1E5
-    output_df['weekly_pc_increase'] = output_df['NewCase_Rel'] * 100
-    output_df['weekly_pc_change'] = output_df['NewCase_PercentChange'] * 100
+    output_df=output_df.rename(columns={'NewCase':'weekly_new_cases','NewDeath':'weekly_new_deaths',\
+                                        'CumCase':'cumulative_cases','CumDeath':'cumulative_deaths'})
+    output_df['weekly_new_cases_per_ht'] = output_df['weekly_new_cases'] / output_df['population'] * 1E5
+    output_df['weekly_new_deaths_per_ht'] = output_df['weekly_new_deaths'] / output_df['population'] * 1E5
+    output_df['weekly_new_cases_pc_change'] = output_df['NewCase_PercentChange'] * 100
+    output_df['weekly_new_deaths_pc_change'] = output_df['NewDeath_PercentChange'] * 100
 
     # Show plots
     pd.plotting.register_matplotlib_converters()
-    for q in ['weekly_new_cases_per_ht', 'weekly_pc_change']:
+    for q in ['weekly_new_cases_per_ht', 'weekly_new_cases_pc_change','weekly_new_deaths_per_ht', 'weekly_new_deaths_pc_change']:
         fig,axs=plt.subplots(figsize=[15,10],nrows=8,ncols=8)
         fig.suptitle(q)
         ifig = 0
         for iso3, group in output_df.groupby('ISO_3_CODE'):
             axis = axs[ifig // 8][ifig % 8]
-            if q == 'weekly_pc_change':
+            if q in ['weekly_new_cases_pc_change','weekly_new_deaths_pc_change']:
                 idx = group[q] > 0
                 axis.bar(x=group['date_epicrv'][idx], height=group[q][idx], color='r')
                 idx = group[q] < 0
@@ -74,13 +80,13 @@ def main():
             axis.set_title(iso3)
             axis.set_xticks([])
             ifig += 1
-    plt.show()
 
     # Save as JSON
     output_df['date_epicrv'] = output_df['date_epicrv'].apply(lambda x: x.strftime('%Y-%m-%d'))
-    output_df = output_df.drop(['NewCase_Rel', 'NewCase_PercentChange', 'weekly_pc_increase', 'ndays', 'diff'], axis=1)
+    output_df = output_df.drop(['NewCase_PercentChange','NewDeath_PercentChange', 'ndays', 'diff_cases','diff_deaths'], axis=1)
     output_df.groupby('ISO_3_CODE').apply(lambda x: x.to_dict('r')).to_json(
         'hrp_covid_weekly_trend.json', orient='index', indent=2)
+    plt.show()
 
 
 def get_WHO_data(HRP_iso3):
@@ -88,7 +94,7 @@ def get_WHO_data(HRP_iso3):
     # get only HRP countries
     df = df.loc[df['ISO_3_CODE'].isin(HRP_iso3),:]
     df['date_epicrv']=pd.to_datetime(df['date_epicrv'])
-    df=df[['date_epicrv','ISO_3_CODE','CumCase','NewCase']]
+    df=df[['date_epicrv','ISO_3_CODE','CumCase','NewCase','NewDeath','CumDeath']]
 
     # adding global by date
     df_all=df.groupby('date_epicrv').sum()
