5092c13fd5b9a231b458c9b0290cd159c1b4813e
Leonardo Milano <milanoleonardo@yahoo.it>
2020-07-15 10:45:13 +0200

    adding H25 data

--- calculate_weekly_increase.py
+++ calculate_weekly_increase.py
@@ -11,6 +11,7 @@ DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 WHO_COVID_FILENAME='WHO_data/Data_ WHO Coronavirus Covid-19 Cases and Deaths - WHO-COVID-19-global-data.csv'
 WHO_COVID_URL='https://docs.google.com/spreadsheets/d/e/2PACX-1vSe-8lf6l_ShJHvd126J-jGti992SUbNLu-kmJfx1IRkvma_r4DHi0bwEW89opArs8ZkSY5G2-Bc1yT/pub?gid=0&single=true&output=csv'
 POPULATION_FILENAME='Population_data/API_SP.POP.TOTL_DS2_en_excel_v2_1121005.xls'
+H25_iso3=['AFG','BDI','BFA','CAF','CMR','COD','COL','ETH','HTI','IRQ','LBY','MLI','MMR','NER','NGA','PSE','SDN','SOM','SSD','SYR','TCD','UKR','VEN','YEM','ZWE']
 
 MIN_CUMULATIVE_CASES = 100
 
@@ -39,14 +40,14 @@ def main(download_covid=False):
     # Read in list of countries
     with open('countries/admins.yaml', 'r') as stream:
         country_list = yaml.safe_load(stream)['admin_info']
-    HRP_iso3 = sorted(list(set([country.get('alpha_3', None) for country in country_list])))
+    H63_iso3 = sorted(list(set([country.get('alpha_3', None) for country in country_list])))
     
     # Download latest covid file tiles and read them in
     if download_covid:
         get_covid_data(WHO_COVID_URL,f'{DIR_PATH}/{WHO_COVID_FILENAME}')
     
     # get WHO data and calculate sum as 'H63'
-    df_WHO=get_WHO_data(HRP_iso3)
+    df_WHO=get_WHO_data(H63_iso3)
     
     # get weekly new cases
     new_w=df_WHO.groupby(['ISO_3_CODE']).resample('W', on='date_epicrv').sum()[['NewCase','NewDeath']]
@@ -69,7 +70,7 @@ def main(download_covid=False):
 
     output_df=output_df[output_df['CumCase']>MIN_CUMULATIVE_CASES]
 
-    df_pop=get_pop_data(HRP_iso3)
+    df_pop=get_pop_data(H63_iso3)
 
     # Add pop to output df
     output_df = output_df.merge(df_pop, left_on='ISO_3_CODE', right_on='Country Code', how='left').drop(
@@ -116,10 +117,10 @@ def main(download_covid=False):
     plt.show()
 
 
-def get_WHO_data(HRP_iso3):
+def get_WHO_data(H63_iso3):
     df=pd.read_csv(f'{DIR_PATH}/{WHO_COVID_FILENAME}')
     # get only HRP countries
-    df = df.loc[df['ISO_3_CODE'].isin(HRP_iso3),:]
+    df = df.loc[df['ISO_3_CODE'].isin(H63_iso3),:]
     df['date_epicrv']=pd.to_datetime(df['date_epicrv'])
     df=df[['date_epicrv','ISO_3_CODE','CumCase','NewCase','NewDeath','CumDeath']]
 
@@ -128,34 +129,45 @@ def get_WHO_data(HRP_iso3):
     df_H63['ISO_3_CODE']='H63'
     df_H63=df_H63.reset_index()
 
+    # adding global by date
+    df_H25=df.loc[df['ISO_3_CODE'].isin(H25_iso3),:]
+    df_H25=df.groupby('date_epicrv').sum()
+    df_H25['ISO_3_CODE']='H25'
+    df_H25=df_H25.reset_index()
+
     # adding regional by date
-    dict_regions=get_dict_regions(HRP_iso3)
+    dict_regions=get_dict_regions(H63_iso3)
     df=pd.merge(left=df,right=dict_regions,left_on='ISO_3_CODE',right_on='ISO3',how='left')
     df=df.drop(labels='ISO3',axis='columns')
     df_regional=df.groupby(['date_epicrv','Regional_office']).sum().reset_index()
     df_regional=df_regional.rename(columns={'Regional_office':'ISO_3_CODE'})    
 
     df=df.append(df_H63)
+    df=df.append(df_H25)
     df=df.append(df_regional)
     return df
 
-def get_dict_regions(HRP_iso3):
+def get_dict_regions(H63_iso3):
     dict_regions=pd.read_csv('countries/tbl_regcov_2020_ocha.csv')
     dict_regions=dict_regions[['ISO3','Regional_office']]
-    dict_regions=dict_regions[dict_regions['ISO3'].isin(HRP_iso3)]
+    dict_regions=dict_regions[dict_regions['ISO3'].isin(H63_iso3)]
     return dict_regions
 
 
-def get_pop_data(HRP_iso3):
+def get_pop_data(H63_iso3):
     # Read in pop
     df_pop=pd.read_excel(POPULATION_FILENAME,sheet_name='Data',header=1,skiprows=[0,1],usecols='B,BK').rename(
         columns={'2018': 'population'})
-    # Add HRP
+    # Add H63
     df_pop = df_pop.append({'Country Code': 'H63',
-                            'population':  df_pop.loc[df_pop['Country Code'].isin(HRP_iso3), 'population'].sum()},
+                            'population':  df_pop.loc[df_pop['Country Code'].isin(H63_iso3), 'population'].sum()},
+                           ignore_index=True)
+    # Add H25
+    df_pop = df_pop.append({'Country Code': 'H25',
+                            'population':  df_pop.loc[df_pop['Country Code'].isin(H25_iso3), 'population'].sum()},
                            ignore_index=True)
     # add regions
-    dict_regions=get_dict_regions(HRP_iso3)
+    dict_regions=get_dict_regions(H63_iso3)
     df=pd.merge(left=df_pop,right=dict_regions,left_on='Country Code',right_on='ISO3',how='left')
     df=df.drop(labels='ISO3',axis='columns')
     df_regional=df.groupby(['Regional_office']).sum().reset_index()
